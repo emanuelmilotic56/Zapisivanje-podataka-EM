a02db13f55b38dd76ce03bbfc37af6add20f8e2b
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-04-12 09:25:39 +0200

    Add files via upload

---
+++ PrezimeImeSystemNiOP-EM/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrezimeImeSystemNiOP_EM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zapis = "";
+            string path = @"D:\Documents\TestNiop.csv ";
+
+            Console.Write("Upisi ime: ");
+
+            Ime = Convert.ToString(Console.ReadLine()) + System.Environment.NewLine;
+            Console.Write("Upiši Prezime: ");
+
+            Prezime = Console.ReadLine() + System.Environment.NewLine;
+            Console.Write("Upisi email: ");
+            Email = Console.ReadLine() + System.Environment.NewLine;
+
+            Console.Write("Upisi godinu rodjenja ");
+            GodRodjenja = Console.ReadLine() + System.Environment.NewLine;
+
+
+
+        }
+    }
+}
